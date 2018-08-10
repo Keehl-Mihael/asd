b90f4fceef52fec2dcf8aa469e6c4c3f0ec9ba61
arkango <asaracino@aryma.it>
2018-08-10 19:58:31 +0200

    creazione prototipo

---
+++ esercizio2-1.c
@@ -0,0 +1,21 @@
+/*
+Scrivere una frunzione ricorsiva che, dati due vettori ordinati vet1 e vet2, 
+di dimensione r1 ed r2, li fonda, ottenendo il vettore temp, anch'esso orordinato.
+Sia il prototipo della funzione 
+    int merge(int vet1[],int vet2[],int r1,int r2,int temp[] )
+*/
+
+int main(){
+    int r1 = 4,r2 = 3;
+    int vet1[] = {1,2,3,4,5};
+    int vet2[] = {7,9,1,1};
+
+    merge(vet1,vet2,r1,r2);
+
+
+    return 0;
+}
+
+int merge(){
+    
+}
