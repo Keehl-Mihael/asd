a4897cd1e9065567f7618f7499d3dd01182648f9
Arcangelo Saracino <saracinoarcangelo@gmail.com>
2018-10-26 10:19:07 +0200

    Create listaSinglePointerFlag.h

---
+++ listaTemplate/listaSinglePointerFlag.h
@@ -0,0 +1,33 @@
+#ifndef ASD_LISTASINGLEPOINTERFLAG_H
+#define ASD_LISTASINGLEPOINTERFLAG_H
+#inlcude "listaSinglePOinter.h"
+class listasinglepointerflag : public listsinglepointer<T>{
+  public :
+    void insLista(const value_type a, postion  p){
+      if(!finelista(p)){
+        super::inslista(a,p);
+      }else{
+        position prev;
+        position t = new cella<T>;
+        t->value = a;
+        t->succ = head; 
+        prev = predLista(p)
+        prev->succ = t;
+        lenght ++;
+      }
+    }
+    void canclista(potion p){
+      if(!finelista(p)){
+        super::canclista(p);
+      }else{
+            postion prev;
+            if(!listaVuota())
+                prev = predLista(p);
+                prev->succ = head;
+                delete[] p;
+                lenght --;
+      }
+    }
+  
+};
+#endif // ASD_LISTASINGLEPOINTERFLAG_H
