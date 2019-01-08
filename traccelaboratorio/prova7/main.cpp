a93e278d387d47ed92916898e6619725d78c4e71
arkango <asaracino@aryma.it>
2019-01-08 12:07:39 +0100

    prova 7 iniziata,prova 4 da completare

--- traccelaboratorio/prova7/main.cpp
+++ traccelaboratorio/prova7/main.cpp
@@ -6,32 +6,46 @@
 
 #include <iostream>
 using namespace std;
-#include "../../alberibinari/alberiPuntatori.h"
+#include "../../grafi/GrafoList.h"
 #include "richieste.h"
 
 int main(){
     richieste<int> rich;
-    alberiPuntatori<int> T;
-    typename alberiPuntatori<int>::value_type v1;
-    typename alberiPuntatori<int>::Nodo n1, n2;
+    GrafoList<int,int> G(10);
+    typedef typename GrafoList<string,double>::Nodo NodoG;
 
-    v1 = 1;
 
-    T.ins_root(n1);
-    T.write(T.root(),1);
-    n1 = T.root();
-    T.ins_sx(n1);
-    T.ins_dx(n1);
-    T.write(T.sx(n1),2);
-    n1 = T.dx(n1);
-    T.write(n1, 3);
-    T.ins_dx(n1);
-    T.write(T.dx(n1), 4);
 
-    T.print();
+    NodoG n1,n2,n3,n4,n5,n6;
 
-    cout << rich.odd(T,2) << endl;
-    cout << rich.even_leafs(T) << endl;
+    G.insNodo(n1);
+    G.scriviEtichetta(n1,1);
+    G.insNodo(n2);
+    G.scriviEtichetta(n2,3);
+    G.insNodo(n3);
+    G.scriviEtichetta(n3,5);
+    G.insNodo(n4);
+    G.scriviEtichetta(n4,4);
+    G.insNodo(n5);
+    G.scriviEtichetta(n5,6);
+    G.insNodo(n6);
+    G.scriviEtichetta(n6,2);
+
+
+
+    G.insArco(n1,n2, 1);
+    G.insArco(n1,n3, 9);
+    G.insArco(n1,n5, 3);
+    G.insArco(n2,n4, 1);
+    G.insArco(n2,n6, 2);
+    G.insArco(n3,n4, 10);
+    G.insArco(n3,n6, 7);
+    G.insArco(n4,n1, 5);
+    G.insArco(n5,n2, 4);
+    G.insArco(n5,n3, 2);
+    G.insArco(n6,n4, 1);
+
+    rich.printAllPaths(5,3);
 
     return 0;
 }
