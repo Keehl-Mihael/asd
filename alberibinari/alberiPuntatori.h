2792d14d37f4be4f8d22b1aab0e5f90ba43fe492
arkango <asaracino@aryma.it>
2019-01-25 11:27:13 +0100

    alberipuntaotri migliorato ins dx in sx

--- alberibinari/alberiPuntatori.h
+++ alberibinari/alberiPuntatori.h
@@ -111,9 +111,13 @@ public:
     };
     void ins_sx(Nodo n){
         n->sxn = new cella<T>;
+        n->sxn->sxn = NULL;
+        n->sxn->dxn = NULL;
     };
     void ins_dx(Nodo n){
         n->dxn = new cella<T>;
+        n->dxn->sxn = NULL;
+        n->dxn->dxn = NULL;
     };
     void previsit(Nodo n){
         if (n == NULL) return;
